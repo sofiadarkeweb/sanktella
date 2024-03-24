cf8a6f8dbd6b02632c6a6f5b6dee735eb8992a94
Sofia Darke <sofia.darke@tradera.com>
2024-03-24 08:54:54 +0100

    init sanktella

---
+++ sanktella/src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <h1>Sanktella</h1> */}
+      <p className="texter">texter</p>
+      <span className="forum">forum</span>
+      <span className="merch">merch</span>
+      <p className="uppdrag">uppdrag</p>
+    </>
+  );
+}
+
+export default App;
